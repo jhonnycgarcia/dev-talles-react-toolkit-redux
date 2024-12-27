a8a708a1fe2f134127d150ea17860fcfaf5dde77
Jhonny <jhonnycgarcia@gmail.com>
2024-12-27 00:37:06 -0500

    RTK Query

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -14,25 +14,25 @@ export const PokemonApp = () => {
 
   return (
     <>
-        <h1>Pokemon App</h1>
-        <hr />
+      <h1>Pokemon App</h1>
+      <hr />
 
-        <span>
-          Loading: {isLoading ? 'True' : 'False'}
-        </span>
-        <ul>
-          {
-            pokemons.map((pokemon) => (
-              <li key={pokemon.name}>{pokemon.name}</li>
-            ))
-          }
-        </ul>
-        <button 
-          disabled={isLoading}
-          onClick={() => dispatch(getPokemons(page))}
-        >
-          Next
-        </button>
+      <span>
+        Loading: {isLoading ? 'True' : 'False'}
+      </span>
+      <ul>
+        {
+          pokemons.map((pokemon) => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))
+        }
+      </ul>
+      <button
+        disabled={isLoading}
+        onClick={() => dispatch(getPokemons(page))}
+      >
+        Next
+      </button>
     </>
   )
 }
