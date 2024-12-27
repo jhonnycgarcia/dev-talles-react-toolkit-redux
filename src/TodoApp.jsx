74e34234aed0c2ac20d75a0a46d80eb64751559f
Jhonny <jhonnycgarcia@gmail.com>
2024-12-27 00:45:54 -0500

    Consumir el API mediante el custom hook

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,11 +1,22 @@
+import { useGetTodosQuery } from "./store/apis"
 
 export const TodoApp = () => {
+  const { data: todos = [], isLoading, error } = useGetTodosQuery();
   return (
     <>
       <h1>Todo App - RTK Query</h1>
       <hr />
-      <h4>isLoading...</h4> 
-      <pre>...</pre>
+      <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
+      <ul>
+        {
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <strong>{todo.completed ? '✅' : '⬜️'}</strong>
+              {todo.title}
+            </li>
+          ))
+        }
+      </ul>
       <button>Next Todo</button>
     </>
   )
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { counterSlice } from './slices/counter'
 import { pokemonSlice } from './slices/pokemon'
+import { todosApi } from './apis'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
     pokemons: pokemonSlice.reducer,
+
+    [todosApi.reducerPath]: todosApi.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todosApi.middleware),
 })
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+// setupListeners(store.dispatch)
