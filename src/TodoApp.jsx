e5755b2c7482fdc2b655a29bca089e71f92cb22b
Jhonny <jhonnycgarcia@gmail.com>
2024-12-27 00:55:49 -0500

    Obtener un Todo por ID

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,13 +1,35 @@
-import { useGetTodosQuery } from "./store/apis"
+import { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis"
 
 export const TodoApp = () => {
-  const { data: todos = [], isLoading, error } = useGetTodosQuery();
+  const [todoId, setTodoId] = useState(1)
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  }
+
+  const prevTodo = () => {
+    if (todoId === 1) return;
+    setTodoId(todoId - 1);
+  }
+
   return (
     <>
       <h1>Todo App - RTK Query</h1>
       <hr />
       <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
-      <ul>
+
+      <pre>{JSON.stringify(todo, null, 2)}</pre>
+      <button onClick={prevTodo} disabled={todoId === 1}>
+        Prev Todo
+      </button>
+      <button onClick={nextTodo}>
+        Next Todo
+      </button>
+
+      {/* <ul>
         {
           todos.map((todo) => (
             <li key={todo.id}>
@@ -16,8 +38,7 @@ export const TodoApp = () => {
             </li>
           ))
         }
-      </ul>
-      <button>Next Todo</button>
+      </ul> */}
     </>
   )
 }
